92b39892d82cf70440b44c533f895b858d7a5de5
Derick <derickvanpascual@gmail.com>
2023-06-23 14:40:25 +0200

    Added process.env.PORT

--- server.js
+++ server.js
@@ -56,6 +56,6 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
     console.log(`Server listening on port ${port}`);
 });
