6bdba282f1310e04c2749023bf550938d0aaab31
Derick <derickvanpascual@gmail.com>
2023-06-23 11:04:44 +0200

    Removed console log

--- twilio/verification.js
+++ twilio/verification.js
@@ -3,8 +3,7 @@ const { client, serviceSid } = require('./twilio');
 const sendVerificationText = (phoneNumber) => {
     client.verify.v2.services(serviceSid)
         .verifications
-        .create({ to: phoneNumber, channel: 'sms' })
-        .then(verification => console.log(verification.status));
+        .create({ to: phoneNumber, channel: 'sms' });
 };
 
 const checkVerificationText = (phoneNumber, code) => {
